a95bb09ecd552e6f5faeac9857d4a56f3b326b36
yujin-1092 <kunche1092@gmail.com>
2024-12-13 18:21:14 +0900

    게시글 상세보기 php 기초 틀 작성

---
+++ community/community_view.php
@@ -0,0 +1,12 @@
+<?php 
+$title = "게시글 상세보기";
+$community_css = "<link href=\"http://{$_SERVER['HTTP_HOST']}/qc/css/community.css\" rel=\"stylesheet\">";
+
+include_once($_SERVER['DOCUMENT_ROOT'] . '/qc/inc/header.php');
+
+?>
+
+
+<?php
+include_once($_SERVER['DOCUMENT_ROOT'] . '/qc/inc/footer.php');
+?>
